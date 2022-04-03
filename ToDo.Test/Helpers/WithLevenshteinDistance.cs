560b8600a7be537a17c0a9a851b5749173aa3609
Jerome Manzano <jeromemanzano@gmail.com>
2022-04-03 18:40:42 +0800

    Add unit tests

---
+++ ToDo.Test/Helpers/WithLevenshteinDistance.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDo.Helpers;
+
+namespace ToDo.Test.Helpers
+{
+    [TestClass]
+    public class WithLevenshteinDistance
+    {
+        private IStringMetric _stringMetric;
+        
+        [TestInitialize]
+        public void Init()
+        {
+            _stringMetric = new LevenshteinDistance();
+        }
+        
+        [DataRow("aa")]
+        [DataRow("")]
+        [DataRow("a")]
+        [DataTestMethod]
+        public void When_FirstString_Length_Is_Zero_Should_Return_SecondString_Length(string secondString)
+        {
+            var distance = _stringMetric.Compute(string.Empty, secondString);
+            Assert.AreEqual(secondString.Length, distance);
+        }
+        
+        [DataRow("aa")]
+        [DataRow("")]
+        [DataRow("a")]
+        [DataTestMethod]
+        public void When_StringSecond_Length_Is_Zero_Should_Return_FirstString_Length(string firstString)
+        {
+            var distance = _stringMetric.Compute(firstString, String.Empty);
+            Assert.AreEqual(firstString.Length, distance);
+        }
+        
+        
+        [DataRow("climax", "volmax", 3)]
+        [DataRow("Ram", "Raman", 2)]
+        [DataRow("mama", "mom", 2)]
+        [DataTestMethod]
+        public void Should_Return_ExpectedDistance(string firstString, string secondString, int expectedDistance)
+        {
+            var distance = _stringMetric.Compute(firstString, secondString);
+            Assert.AreEqual(expectedDistance, distance);
+        }
+    }
+}
---
+++ ToDo.Test/ViewModels/WithMainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDo.ViewModels;
+
+namespace ToDo.Test.ViewModels
+{
+    [TestClass]
+    public class WithMainViewModel
+    {
+        private MainViewModel _viewModel;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _viewModel = new MainViewModel();
+        }
+
+        [DataRow(null)]
+        [DataRow("")]
+        [DataRow("a")]
+        [DataTestMethod]
+        public void AddItemCommand_Should_Not_Execute_When_NewItem_Is_Null_Or_Empty(string newItem)
+        {
+            _viewModel.NewItem = newItem;
+            Assert.AreNotEqual(string.IsNullOrEmpty(newItem), _viewModel.AddItemCommand.CanExecute(null));
+        }
+
+        [TestMethod]
+        public void AddItemCommand_Should_Add_NewItem_To_ToDoList_And_Clear_NewItem()
+        {
+            var itemToAdd = "Test item";
+            _viewModel.NewItem = itemToAdd;
+            
+            _viewModel.AddItemCommand.Execute(null);
+            
+            CollectionAssert.Contains(_viewModel.ToDoList, itemToAdd);
+            Assert.AreEqual(string.Empty, _viewModel.NewItem);
+        }
+
+        [TestMethod]
+        public void When_NewItem_Changed_Should_Call_PropertyChanged_And_NotifyCanExecuteChanged()
+        {
+            var propertyChangedCounter = 0;
+            var notifyCanExecuteChanged = 0;
+
+            _viewModel.PropertyChanged += (sender, args) =>
+            {
+                propertyChangedCounter++;
+            };
+
+            _viewModel.AddItemCommand.CanExecuteChanged += (sender, args) =>
+            {
+                notifyCanExecuteChanged++;
+            };
+
+            _viewModel.NewItem = "Test";
+            
+            Assert.AreEqual(1, propertyChangedCounter);
+            Assert.AreEqual(1, notifyCanExecuteChanged);
+        }
+    }
+}
